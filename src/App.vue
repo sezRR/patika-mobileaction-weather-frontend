3f6aa3c6ce6bcd6bc57e67a7bc5d07864c6d382a
Sezer Tetik <sezer.tetik2005@gmail.com>
2025-08-20 21:24:48 +0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { MaButton } from "@mobileaction/action-kit";
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const incrementCounter = () => {
+    counter.value++;
+};
+</script>
+
+<style src="@/styles/main.css" />
+
+<template>
+    <div class="h-dvh flex justify-center items-center flex-col gap-4">
+        <span class="font-bold text-2xl">Hello World!</span>
+        <MaButton :onClick="incrementCounter">Click Me</MaButton>
+        <span class="text-gray-500 text-sm">
+            {{ counter }}
+        </span>
+    </div>
+</template>
